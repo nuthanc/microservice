8d4a1af20eeb9f03da52922c1ff3aa6575bb2709
Nuthan Chandra <nuthanc@juniper.net>
2021-01-14 21:08:28 +0530

    Custom Event Interface updated

--- ticketing/nats-test/src/events/ticket-created-event.ts
+++ ticketing/nats-test/src/events/ticket-created-event.ts
@@ -1,10 +1,10 @@
 import { Subjects } from './subjects';
 
 export interface TicketCreatedEvent {
-  subject: Subjects.TicketCreated,
+  subject: Subjects.TicketCreated;
   data: {
-    id: string,
-    title: string,
-    price: number
+    id: string;
+    title: string;
+    price: number;
   }
 }
