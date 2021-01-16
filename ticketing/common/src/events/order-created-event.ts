a706e92d2c10b6dab47c661b8591d93b01b5b117
Nuthan Chandra <nuthanc@juniper.net>
2021-01-16 08:58:17 +0530

    Updates

---
+++ ticketing/common/src/events/order-created-event.ts
@@ -0,0 +1,12 @@
+import { Subjects } from './subjects';
+import { OrderStatus } from './types/order-status';
+
+export interface OrderCreatedEvent {
+  subject: Subjects.OrderCreated;
+  data: {
+    id: string;
+    ticket: {
+      id: string;
+    };
+  };
+}
