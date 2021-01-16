9daf3605ffada3e3b789c7c35d1ad2051c06c32e
Nuthan Chandra <nuthanc@juniper.net>
2021-01-16 09:09:48 +0530

    Updates

--- ticketing/common/src/events/order-cancelled-event.ts
+++ ticketing/common/src/events/order-cancelled-event.ts
@@ -1,16 +1,11 @@
 import { Subjects } from './subjects';
-import { OrderStatus } from './types/order-status';
 
 export interface OrderCancelledEvent {
   subject: Subjects.OrderCancelled;
   data: {
     id: string;
-    status: OrderStatus.Cancelled;
-    userId: string;
-    expiresAt: string;
     ticket: {
       id: string;
-      price: number;
     };
   };
 }
--- ticketing/common/src/events/order-created-event.ts
+++ ticketing/common/src/events/order-created-event.ts
@@ -5,8 +5,12 @@ export interface OrderCreatedEvent {
   subject: Subjects.OrderCreated;
   data: {
     id: string;
+    status: OrderStatus.Created;
+    userId: string;
+    expiresAt: string;
     ticket: {
       id: string;
+      price: number;
     };
   };
 }
