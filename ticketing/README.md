66b6e5fae38615df8d5ab693ac5843e955e1cdfd
Nuthan Chandra <nuthanc@juniper.net>
2021-01-16 09:52:20 +0530

    A Few More Reminders

--- ticketing/README.md
+++ ticketing/README.md
@@ -2784,3 +2784,8 @@ npm update @rztickets/common
 
 ### Blueprint for Listeners
 * ticket-created-listener implementation in orders
+
+### A Few More Reminders
+* Diagram link: https://app.diagrams.net/#Uhttps%3A%2F%2Fraw.githubusercontent.com%2FStephenGrider%2Fmicroservices-casts%2Fmaster%2Fdiagrams%2F08%2F03.drawio
+* D 1-listener:
+  * One of the members of the Queue Group
--- ticketing/orders/src/events/listeners/ticket-created-listener.ts
+++ ticketing/orders/src/events/listeners/ticket-created-listener.ts
@@ -1,12 +1,11 @@
 import { Message } from 'node-nats-streaming';
 import { Subjects, Listener, TicketCreatedEvent } from '@rztickets/common';
 import { Ticket } from '../../models/ticket';
+import { queueGroupName } from './queue-group-name';
 
 export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
   subject: Subjects.TicketCreated = Subjects.TicketCreated;
-  queueGroupName = 'orders-service';
-  
-  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
-    
-  }
+  queueGroupName = queueGroupName;
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {}
 }
