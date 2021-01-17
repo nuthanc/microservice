23d82887ad1dc4d1d69f990b7303356e80cd7482
Nuthan Chandra <nuthanc@juniper.net>
2021-01-17 09:10:00 +0530

    Another Order Model!

--- ticketing/README.md
+++ ticketing/README.md
@@ -3291,3 +3291,6 @@ docker push nuthanc/payments
 * Diagram link: https://app.diagrams.net/#Uhttps%3A%2F%2Fraw.githubusercontent.com%2FStephenGrider%2Fmicroservices-casts%2Fmaster%2Fdiagrams%2F10%2F01.drawio
 * D 1-payments: Events
 * D 4-props:
+
+### Another Order Model!
+* In payments
---
+++ ticketing/payments/src/models/order.ts
@@ -0,0 +1,60 @@
+import mongoose from 'mongoose';
+import { OrderStatus } from '@rztickets/common';
+
+interface OrderAttrs {
+  id: string;
+  userId: string;
+  status: OrderStatus;
+  version: number;
+  price: number;
+}
+
+interface OrderDoc extends mongoose.Document {
+  userId: string;
+  status: OrderStatus;
+  version: number;
+  price: number;
+}
+
+interface OrderModel extends mongoose.Model<OrderDoc> {
+  build(attrs: OrderAttrs): OrderDoc;
+}
+
+const orderSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    status: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
+  }
+);
+
+orderSchema.statics.build = (attrs: OrderAttrs) => {
+  return new Order({
+    _id: attrs.id,
+    version: attrs.version,
+    price: attrs.price,
+    userId: attrs.userId,
+    status: attrs.status
+  });
+};
+
+const Order = mongoose.model<OrderDoc, OrderModel>('Order', orderSchema);
+
+export { Order };
